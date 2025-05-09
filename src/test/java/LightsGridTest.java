c314bb755154b4aed32c938787f9a8f41997cc5f
Emil Hotkowski <ehotkowski@hopper.com>
2025-05-09 22:22:49 +0200

    feat: Added test to toggle OFF some lights

--- src/test/java/LightsGridTest.java
+++ src/test/java/LightsGridTest.java
@@ -5,12 +5,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class LightsGridTest {
 
-    /*
-     * I would expect three things:
-     * - Ability to TURN on lights in a rectangle
-     * - Ability to TOGGLE a whole rectangle
-     * - Ability to get the number of turned on lights in a rectangle (or whole grid)
-     */
     @Test
     void should_turn_on_lights_in_rectangle() {
         LightsGrid lightsGrid = new LightsGrid(3, 3);
@@ -44,5 +38,18 @@ class LightsGridTest {
 
         assertThat(lightsGrid.getLights(new Rectangle(0, 0, 2, 2)))
                 .isEqualTo(4);
+    }
+
+    @Test
+    void should_toggle_off_some_lights_in_rectangle() {
+        LightsGrid lightsGrid = new LightsGrid(3, 3);
+
+        Rectangle rectangleWithLights = new Rectangle(0, 0, 1, 1);
+        lightsGrid.turnOnLights(rectangleWithLights);
+        Rectangle rectangleWithLights2 = new Rectangle(0, 0, 0, 0);
+        lightsGrid.toggleLights(rectangleWithLights2);
+
+        assertThat(lightsGrid.getLights(new Rectangle(0, 0, 2, 2)))
+                .isEqualTo(3);
     }
 }
